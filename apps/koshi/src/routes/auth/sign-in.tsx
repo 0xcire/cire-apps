59e4fde4a3221bb043c6614a3a55b8171abc0141
0xcire <ciredeveloper@gmail.com>
2025-06-26 21:18:57 -0400

    redirect for return

--- apps/koshi/src/routes/auth/sign-in.tsx
+++ apps/koshi/src/routes/auth/sign-in.tsx
@@ -1,9 +1,13 @@
 import { PageWrapper } from "@/components/page/page-wrapper";
 import { SignInFormCard } from "@/features/auth/components/sign-in";
 import { createFileRoute } from "@tanstack/react-router";
+import { z } from "zod";
 
 export const Route = createFileRoute("/auth/sign-in")({
   component: RouteComponent,
+  validateSearch: z.object({
+    redirect: z.union([z.string(), z.undefined()])
+  })
 });
 
 function RouteComponent() {
